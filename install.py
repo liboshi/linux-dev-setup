642617e4501a06eee22b83c3960717f4ed70cd71
Boush Li <li.boush@gmail.com>
2016-05-20 01:30:26 +0800

    o Update for compatibility.

--- install.py
+++ install.py
@@ -7,16 +7,28 @@ TODO:
 '''
 
 import os
+import re
 import sys
+import platform
 import logging
 import subprocess
 
 log = logging.getLogger(__name__)
 
+def which_dist():
+    dist_name = platform.dist()[0]
+    if re.search(r'Ubuntu|debian'):
+        return 'debian'
+    else:
+        return 'redhat'
+
 def make_vim_plugin_folder():
     vim_plugin_folder = os.path.expanduser(r'~/.vim/bundle')
     if not os.path.exists(vim_plugin_folder):
-        os.makedirs(vim_plugin_folder)
+        try:
+            os.makedirs(vim_plugin_folder)
+        except OSError as err:
+            log.error(err)
     else:
         log.info('>>> Plugin folder exists...')
 
@@ -27,10 +39,15 @@ def install_github_bundle(user, package):
 
 def install_tmux():
     log.info('>>> Install tmux')
-    subprocess.call(r'sudo apt-get install -y tmux', shell = True)
+    pkg_cmd = 'apt-get'
+    dist_name = which_dist()
+    if dist_name != 'debian':
+        pkg_cmd = 'yum'
+    subprocess.call(r'sudo {0} install -y tmux'.format(pkg_cmd), shell = True)
 
 def link_file(original_filename, symlink_filename):
     original_path = os.path.expanduser(original_filename)
+    original_path = os.path.join(os.getcwd, original_path)
     symlink_path  = os.path.expanduser(symlink_filename)
     if os.path.exists(symlink_path):
         os.unlink(symlink_path)
