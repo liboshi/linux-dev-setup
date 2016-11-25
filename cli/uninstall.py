9243d2ec93bacdf606709a4f0e9a7c1bd4bb9ddb
Boush Li <li.boush@gmail.com>
2016-11-25 17:32:49 +0800

    o Add uninstall process, fixing #1.

---
+++ cli/uninstall.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+'''
+This script is used to cleanup the installation.
+'''
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+import sys
+import logging
+import subprocess
+
+log = logging.getLogger(__name__)
+console_handler = logging.StreamHandler(sys.stderr)
+
+def setup_logging():
+    root_logger = logging.getLogger()
+    root_logger.addHandler(console_handler)
+    root_logger.setLevel(logging.DEBUG)
+
+    # Disable requests logging
+    logging.getLogger('requests').propagate = False
+
+def unisntall_app(pkg_cmd, app_name):
+    log.info('>>> Uninstall {0}'.format(app_name))
+    subprocess.call(r'sudo {0} purge -y {1}'.format(pkg_cmd, app_name),
+                    shell = True)
+
+def unlink_file(symlink_filename):
+    symlink_path = os.path.expanduser(symlink_filename)
+    if os.path.exists(symlink_path):
+        os.unlink(symlink_path)
+
+def uninstall():
+    setup_logging()
