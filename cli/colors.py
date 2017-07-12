f744f60ae295575012ae250d8a865467e1f21849
Boush Li <boush.li@outlook.com>
2017-07-12 22:41:20 +0800

    o Format code.
    Signed-off-by: Boush Li <boush.li@outlook.com>

--- cli/colors.py
+++ cli/colors.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
+
 NAMES = [
     'grey',
     'red',
@@ -11,28 +12,22 @@ NAMES = [
     'white'
 ]
 
-
 def get_pairs():
     for i, name in enumerate(NAMES):
         yield(name, str(30 + i))
         yield('intense_' + name, str(30 + i) + ';1')
 
-
 def ansi(code):
     return '\033[{0}m'.format(code)
-
 
 def ansi_color(code, s):
     return '{0}{1}{2}'.format(ansi(code), s, ansi(0))
 
-
 def make_color_fn(code):
     return lambda s: ansi_color(code, s)
 
-
 for (name, code) in get_pairs():
     globals()[name] = make_color_fn(code)
-
 
 def rainbow():
     cs = ['cyan', 'yellow', 'green', 'magenta', 'red', 'blue',
